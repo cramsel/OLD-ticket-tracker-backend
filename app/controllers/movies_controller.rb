48f17e60388216d7b3878f2e981e0b27b35a745b
cramsel <Luxicorde@gmail.com>
2022-03-30 10:19:31 -0500

    Add create and search methods for movies

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -3,4 +3,26 @@ class MoviesController < ApplicationController
     movies = Movie.all
     render json: movies
   end
+
+  def search
+    response = HTTP.get("https://api.themoviedb.org/3/search/movie?api_key=#{Rails.application.credentials.movie_api_key}&language=en-US&page=1&include_adult=false&query=#{params[:title]}")
+    data = JSON.parse(response.body)
+    movies = data["movies"]
+    render json: data
+  end
+
+  def create
+    movie = Movie.new(
+      title: params[:title],
+      runtime: params[:runtime],
+      summary: params[:summary],
+      poster_url: params[:poster_url],
+    )
+
+    if movie.save
+      render json: movie
+    else
+      render json: { errors: movie.errors.full_messages }
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,10 @@ Rails.application.routes.draw do
   get "/movies" => "movies#index"
   get "/reports" => "reports#index"
 
+  # get "/movies" => "movies#search"
+  # #kept in for testing purposes
+
   post "/sales" => "sales#create"
   post "/reports" => "reports#create"
+  post "/movies" => "movies#create"
 end
